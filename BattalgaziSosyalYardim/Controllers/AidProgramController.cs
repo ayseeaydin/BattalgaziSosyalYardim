329922a92c00ca3112d7a83842562e38a8f4f859
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-08-17 02:26:00 +0300

    Commit

---
+++ BattalgaziSosyalYardim/Controllers/AidProgramController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BattalgaziSosyalYardim.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BattalgaziSosyalYardim.Controllers
+{
+    public class AidProgramController : Controller
+    {
+        private readonly AppDbContext _db;
+        public AidProgramController(AppDbContext db) => _db = db;
+
+        // GET: /AidProgram
+        // Amaç; seed edilen yardım programlarını listelemek
+        public async Task<IActionResult> Index()
+        {
+            var items = await _db.AidPrograms
+                .AsNoTracking()
+                .OrderByDescending(x => x.IsActive)
+                .ThenBy(x => x.Name)
+                .ToListAsync();
+
+            return View(items);
+        }
+    }
+}
