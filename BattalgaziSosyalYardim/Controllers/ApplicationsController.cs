4435371ae528a96c2178f77c4972ccca6ddfbb86
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-08-17 22:31:44 +0300

    Commit

--- BattalgaziSosyalYardim/Controllers/ApplicationsController.cs
+++ BattalgaziSosyalYardim/Controllers/ApplicationsController.cs
@@ -1,35 +1,20 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using BattalgaziSosyalYardim.Database;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BattalgaziSosyalYardim.Controllers
 {
     public class ApplicationsController : Controller
     {
-        private readonly AppDbContext _db;
-        public ApplicationsController(AppDbContext db) => _db = db;
-
-        // GET: /Applications/Create?programCode=bez-destegi
-        public async Task<IActionResult> Create(string? programCode)
+        [HttpGet]
+        public IActionResult Create(string? programCode = null)
         {
-            // İsteğe bağlı: program adını başlığa yazmak istersen:
-            if (!string.IsNullOrWhiteSpace(programCode))
-            {
-                var program = await _db.AidPrograms
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(p => p.Code == programCode);
-
-                ViewData["ProgramTitle"] = program?.Name ?? "Bebek Bezi Desteği";
-            }
-            else
-            {
-                ViewData["ProgramTitle"] = "Bebek Bezi Desteği";
-            }
+            // ekranda başlık vs. için istersen kullanırsın
+            ViewData["ProgramTitle"] = programCode == "bez-destegi"
+                ? "Bebek Bezi Desteği"
+                : "Başvuru Formu";
 
             return View();
         }
+
+        // İleride POST action ekleyeceğiz (şimdilik görünüm odaklıyız)
     }
 }
