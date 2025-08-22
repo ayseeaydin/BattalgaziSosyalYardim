4a5fc3746b0805da7d5e40739cb14dd7dba565db
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-08-22 03:20:33 +0300

    Commit

---
+++ BattalgaziSosyalYardim/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BattalgaziSosyalYardim.Database;
+using BattalgaziSosyalYardim.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace BattalgaziSosyalYardim.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public AdminController(AppDbContext db) => _db = db;
+        
+        // GET: /Admin
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Pending = await _db.Applications.CountAsync(a => a.Status == ApplicationStatus.Pending);
+            ViewBag.Approved = await _db.Applications.CountAsync(a => a.Status == ApplicationStatus.Approved);
+            ViewBag.Rejected = await _db.Applications.CountAsync(a => a.Status == ApplicationStatus.Rejected);
+            return View();
+        }
+    }
+}
